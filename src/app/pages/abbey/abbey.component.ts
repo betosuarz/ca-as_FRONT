aabf9051922a9b9a36bd89d9547de3dff315ec07
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-06-30 03:04:04 +0200

    Creación de componentes del proyecto

---
+++ src/app/pages/abbey/abbey.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abbey',
+  standalone: true,
+  imports: [],
+  templateUrl: './abbey.component.html',
+  styleUrl: './abbey.component.css'
+})
+export class AbbeyComponent {
+
+}

aa00d43fef595bee63c6f442145cfdce1df35ca9
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-06-30 02:27:22 +0200

    Creación de proyecto y configuración inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'canas_FRONT';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }

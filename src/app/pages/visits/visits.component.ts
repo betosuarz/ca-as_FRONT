e20aa02317cb688fe3d3e3d600c16f388db78c44
Ibon Mugica <iboedu@hotmail.com>
2024-07-04 20:55:57 +0200

    Prueba de children
    Unos componentes

---
+++ src/app/pages/visits/visits.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-visits',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './visits.component.html',
+  styleUrl: './visits.component.css'
+})
+export class VisitsComponent {
+
+}

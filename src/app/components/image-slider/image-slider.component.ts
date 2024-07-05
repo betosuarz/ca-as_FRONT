ca9a304f833c58703b8b4238407811d21661aa0e
ILMPI <eguzkia6@gmail.com>
2024-07-06 01:01:11 +0200

    add: image-slider, added transitions, full screen images, count

--- src/app/components/image-slider/image-slider.component.ts
+++ src/app/components/image-slider/image-slider.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
 
 @Component({
   selector: 'app-image-slider',
@@ -8,22 +9,55 @@ import { Component } from '@angular/core';
   templateUrl: './image-slider.component.html',
   styleUrl: './image-slider.component.css'
 })
-export class ImageSliderComponent {
-  
-  // slides = [
-  //   { image: 'assets/gallery/sepulcro/SEPULCRO-1-scaled.jpg', text: '1' },
-  //   { image: 'assets/gallery/sepulcro/SEPULCRO-2-scaled.jpg', text: '2' },
-  //   { image: 'src/assets/gallery/sepulcro/SEPULCRO-25-scaled.jpg', text: '3' },
-  // ];
-  // currentSlide = 0;
+export class ImageSliderComponent implements OnInit {
+  slides = [
+    { image: 'assets/gallery/sepulcro/SEPULCRO-1-scaled.jpg' },
+    { image: 'assets/gallery/sepulcro/SEPULCRO-2-scaled.jpg' },
+    { image: 'assets/gallery/sepulcro/SEPULCRO-25-scaled.jpg' },
+  ];
 
-  // nextSlide(): void {
-  // }
+  currentIndex = 0;
 
-  // previousSlide(): void {
-  // }
+  ngOnInit(): void {
+    this.updateImageSize();
+  }
 
-  // goToSlide(index: number): void {
-  // }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.updateImageSize();
+  }
 
+  updateImageSize() {}
+
+  goToSlide(index: number) {
+    const previousIndex = this.currentIndex;
+    this.currentIndex = index;
+    
+    gsap.to(`.carousel-image-${previousIndex}`, {
+      opacity: 0,
+      scale: 1.05,
+      duration: 2, // 2 секунды
+      ease: 'power2.inOut',
+      onComplete: () => {
+        gsap.to(`.carousel-image-${this.currentIndex}`, {
+          opacity: 1,
+          scale: 1,
+          duration: 2, // 2 секунды
+          ease: 'power2.inOut'
+        });
+      }
+    });
+  }
+
+  nextSlide() {
+    const nextIndex = (this.currentIndex + 1) % this.slides.length;
+    this.goToSlide(nextIndex);
+  }
+
+  previousSlide() {
+    const prevIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+    this.goToSlide(prevIndex);
+  }
+
+  closeCarousel() {}
 }

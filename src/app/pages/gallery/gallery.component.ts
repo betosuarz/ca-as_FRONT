43f81e3ada5285aee56b319413c5aeffccc0784c
ILMPI <eguzkia6@gmail.com>
2024-07-10 21:31:30 +0200

    add: change of favicon

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { ImageSliderComponent } from '../../components/image-slider/image-slider.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [ImageSliderComponent],
+  imports: [ImageSliderComponent, CommonModule],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+  categories = [
+    { name: 'Exterior', image: 'assets/gallery/exterior/CANAS-EXTERIOR-3-scaled.jpg' }
+  ];
 
 }

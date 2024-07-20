6028d43e8af7e3ebd6992d81c72d9c064ec89206
ILMPI <eguzkia6@gmail.com>
2024-07-20 18:33:11 +0200

    add: image gallery, fit: cover and more height

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -14,6 +14,7 @@ import { ICategory } from '../../interfaces/icategory';
   styleUrls: ['./gallery.component.css'],
   providers: [TitleCasePipe]
 })
+
 export class GalleryComponent implements OnInit {
   categories: ICategory[] = [];
   images: string[] = [];

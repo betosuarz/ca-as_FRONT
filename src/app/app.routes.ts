e351e89c0b0dc5e195a3a70084f71f1c6a87f8e8
ILMPI <eguzkia6@gmail.com>
2024-07-22 23:52:01 +0200

    change: redirect from ' ' to /home and hide footer in app.html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,7 +15,7 @@ import { VisitsComponent } from './pages/visits/visits.component';
 
 
 export const routes: Routes = [
-  // { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'abbey', component: AbbeyComponent },
   { path: 'visits', component: VisitsComponent },

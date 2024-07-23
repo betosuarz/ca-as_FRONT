9f9dea528fdb14aaeaf07b0d82b217abdd42ec8d
ILMPI <eguzkia6@gmail.com>
2024-07-23 19:52:09 +0200

    change: header is now consist a few components

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component} from '@angular/core';
+import { RouterLink, NavigationEnd, Event } from '@angular/router';
+import { CarouselComponent } from '../head/carousel/carousel.component';
+import { CommonModule } from '@angular/common';
+import { HorarioComponent } from '../head/horario/horario.component';
+import { MenuComponent } from '../head/menu/menu.component';
+import { MonasteryInfoComponent } from '../head/monastery-info/monastery-info.component';
+import { PoliticsComponent } from '../head/politics/politics.component';
+import { SocialMediaComponent } from '../head/social-media/social-media.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink,
+           CommonModule,
+           CarouselComponent,
+           HorarioComponent,
+           MenuComponent,
+           MonasteryInfoComponent,
+           PoliticsComponent,
+           SocialMediaComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent{
 
 }

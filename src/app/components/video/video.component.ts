7c639c22721765025f71e9cebc57d36e0cf57829
ILMPI <eguzkia6@gmail.com>
2024-07-28 22:05:19 +0200

    add: buttons to stop and play

--- src/app/components/video/video.component.ts
+++ src/app/components/video/video.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild, AfterViewInit, ChangeDetectorRef  } from '@angular/core';
 
 @Component({
   selector: 'app-video',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './video.component.html',
   styleUrl: './video.component.css'
 })
-export class VideoComponent {
+export class VideoComponent implements AfterViewInit {
+  isPlaying = false;
 
+  @ViewChild('videoPlayer') videoPlayer!: ElementRef<HTMLVideoElement>;
+
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.videoPlayer.nativeElement.muted = true;
+    this.playVideo();
+  }
+
+  handleLoadedMetadata(event: Event) {
+    const video = event.target as HTMLVideoElement;
+    video.muted = true;
+  }
+
+  handleCanPlay(event: Event) {
+    const video = event.target as HTMLVideoElement;
+    video.play().catch(error => console.error('Autoplay error:', error));
+  }
+
+  playVideo() {
+    this.videoPlayer.nativeElement.play().then(() => {
+      this.isPlaying = true;
+      this.cdr.detectChanges();
+    }).catch(error => {
+      console.error('Autoplay error:', error);
+    });
+  }
+
+  pauseVideo() {
+    this.videoPlayer.nativeElement.pause();
+    this.isPlaying = false;
+    this.cdr.detectChanges();
+  }
 }

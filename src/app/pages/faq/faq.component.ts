c5e944cf16de376b4e3a900d0686c621d4dd1739
Ibon Mugica <iboedu@hotmail.com>
2024-07-30 22:47:55 +0200

    Arreglos varios

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -1,4 +1,4 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, AfterViewInit, ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
@@ -7,7 +7,50 @@ import { Component, AfterViewInit } from '@angular/core';
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
-export class FaqComponent {
+export class FaqComponent implements AfterViewInit {
 
+  constructor(private cdr: ChangeDetectorRef) {}
+
+  ngAfterViewInit() {
+    this.initializeAccordion();
+  }
+
+  initializeAccordion() {
+    const accordionButtons = document.querySelectorAll('[data-accordion-target]');
+
+    accordionButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        const targetId : any = button.getAttribute('data-accordion-target');
+        const target = document.querySelector(targetId);
+
+        if (target) {
+          const isOpen = target.classList.contains('hidden');
+          this.closeAllAccordions();
+          if (isOpen) {
+            target.classList.remove('hidden');
+            button.setAttribute('aria-expanded', 'true');
+          } else {
+            target.classList.add('hidden');
+            button.setAttribute('aria-expanded', 'false');
+          }
+        }
+      });
+    });
+
+    this.cdr.detectChanges();
+  }
+
+  closeAllAccordions() {
+    const accordionContents = document.querySelectorAll('[id^="accordion-flush-body-"]');
+    const accordionButtons = document.querySelectorAll('[data-accordion-target]');
+
+    accordionContents.forEach(content => {
+      content.classList.add('hidden');
+    });
+
+    accordionButtons.forEach(button => {
+      button.setAttribute('aria-expanded', 'false');
+    });
+  }
 }
 

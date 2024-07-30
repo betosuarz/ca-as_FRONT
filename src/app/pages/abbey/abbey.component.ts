852d098579c05a6220db435217169379ca0dd8df
ILMPI <eguzkia6@gmail.com>
2024-07-30 20:32:40 +0200

    add: side-bar change on clic styles

--- src/app/pages/abbey/abbey.component.ts
+++ src/app/pages/abbey/abbey.component.ts
@@ -6,8 +6,29 @@ import { Component, HostListener, AfterViewInit } from '@angular/core';
   templateUrl: './abbey.component.html',
   styleUrl: './abbey.component.css'
 })
-// export class AbbeyComponent implements AfterViewInit {
-export class AbbeyComponent {
+export class AbbeyComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const observerOptions = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.5
+    };
 
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        const id = entry.target.getAttribute('id');
+        const navLink = document.querySelector(`a[href="abbey#${id}"]`);
+        if (entry.isIntersecting) {
+          navLink?.classList.add('active');
+        } else {
+          navLink?.classList.remove('active');
+        }
+      });
+    }, observerOptions);
+
+    document.querySelectorAll('div[id]').forEach((section) => {
+      observer.observe(section);
+    });
+  }
 
 }

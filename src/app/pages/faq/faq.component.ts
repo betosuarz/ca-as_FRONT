b3293ce0a97ffa4640eb6a8dd11bebacecd4a597
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-07-30 13:33:06 +0200

    Edición estilos faq

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-faq',
   standalone: true,
   imports: [],
   templateUrl: './faq.component.html',
-  styleUrl: './faq.component.css'
+  styleUrls: ['./faq.component.css']
 })
-export class FaqComponent {
+export class FaqComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    this.initializeFlowbiteAccordion();
+  }
+
+  initializeFlowbiteAccordion(): void {
+    // Reemplaza esto con el código de inicialización específico de Flowbite
+    const accordions = document.querySelectorAll('[data-accordion]');
+    accordions.forEach((accordion) => {
+      const targetId = accordion.getAttribute('data-accordion-target');
+      const target = targetId ? document.querySelector(targetId) : null;
+      const isActive = accordion.getAttribute('aria-expanded') === 'true';
+
+      if (isActive && target) {
+        target.classList.remove('hidden');
+      }
+
+      accordion.addEventListener('click', () => {
+        if (target) {
+          const isCurrentlyHidden = target.classList.contains('hidden');
+          if (isCurrentlyHidden) {
+            target.classList.remove('hidden');
+            accordion.setAttribute('aria-expanded', 'true');
+          } else {
+            target.classList.add('hidden');
+            accordion.setAttribute('aria-expanded', 'false');
+          }
+        }
+      });
+    });
+  }
 }
+

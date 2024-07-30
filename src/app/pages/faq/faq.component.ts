6cb9cec9362f062d822c0ab30b9786d17f589c47
Alberto Suárez <alberto.suarez.ing@gmail.com>
2024-07-30 14:04:11 +0200

    Estilos términos y condiciones

--- src/app/pages/faq/faq.component.ts
+++ src/app/pages/faq/faq.component.ts
@@ -7,37 +7,7 @@ import { Component, AfterViewInit } from '@angular/core';
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.css']
 })
-export class FaqComponent implements AfterViewInit {
+export class FaqComponent {
 
-  ngAfterViewInit(): void {
-    this.initializeFlowbiteAccordion();
-  }
-
-  initializeFlowbiteAccordion(): void {
-    // Reemplaza esto con el código de inicialización específico de Flowbite
-    const accordions = document.querySelectorAll('[data-accordion]');
-    accordions.forEach((accordion) => {
-      const targetId = accordion.getAttribute('data-accordion-target');
-      const target = targetId ? document.querySelector(targetId) : null;
-      const isActive = accordion.getAttribute('aria-expanded') === 'true';
-
-      if (isActive && target) {
-        target.classList.remove('hidden');
-      }
-
-      accordion.addEventListener('click', () => {
-        if (target) {
-          const isCurrentlyHidden = target.classList.contains('hidden');
-          if (isCurrentlyHidden) {
-            target.classList.remove('hidden');
-            accordion.setAttribute('aria-expanded', 'true');
-          } else {
-            target.classList.add('hidden');
-            accordion.setAttribute('aria-expanded', 'false');
-          }
-        }
-      });
-    });
-  }
 }
 
